a898f4e515e19ad5182c6a827789ca58ba5cd53c
AGNALDO FRANCISCO DA MOTA <af_mota@hotmail.com>
2025-11-06 14:45:31 +0000

    0.0.08
    exibição da descrição da situação do álbum

--- index.php
+++ index.php
@@ -12,16 +12,18 @@ $sql = "SELECT
             T.id, 
             T.titulo, 
             A.nome AS nome_artista,       
-            TPL.descricao AS descricao_tipo, -- NOVO: Pega a descrição do tipo
+            TPL.descricao AS descricao_tipo,
+            SIT.descricao AS descricao_situacao,  -- NOVO: Pega a descrição da situação
             T.data_lancamento, 
-            T.situacao, 
             T.formato_id
         FROM 
             store AS T
         LEFT JOIN 
             artistas AS A ON T.artista_id = A.id 
-        LEFT JOIN                                   -- NOVO JOIN
+        LEFT JOIN                                   
             tipo_album AS TPL ON T.tipo_id = TPL.id
+        LEFT JOIN                                     -- NOVO JOIN
+            situacao AS SIT ON T.situacao = SIT.id 
         WHERE 
             T.deletado = 0
         LIMIT 100";
@@ -79,11 +81,13 @@ try {
                     <td><?php echo htmlspecialchars($album['nome_artista'] ?? 'Artista Desconhecido'); ?></td>
                     <td><?php echo htmlspecialchars($album['data_lancamento'] ?? 'N/A'); ?></td>
                     <td><?php echo htmlspecialchars($album['descricao_tipo'] ?? 'Não Classificado'); ?></td>
-                    <td><?php echo htmlspecialchars($album['situacao'] ?? 'N/A'); ?></td>
+                    <td><?php echo htmlspecialchars($album['descricao_situacao'] ?? 'Desconhecida'); ?></td>
                     <td><?php echo htmlspecialchars($album['formato_id'] ?? 'N/A'); ?></td>
                     <td></td> 
                 </tr>
-                <?php endforeach; ?>    <?php endif; ?>
-
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
 </body>
 </html>
